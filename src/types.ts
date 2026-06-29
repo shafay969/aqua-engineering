0fde3d8d4c2cf03f1bcd6cc8f3615be942bff840
shafaybilal <shafaybilal123@gmail.com>
2026-06-29 19:25:15 +0800

    Use uploaded hero image

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import heroImage from "../assets/hero image.png";
+
 export interface ServiceOption {
   id: string;
   title: string;
@@ -15,7 +17,7 @@ export interface BookingDetails {
 }
 
 export const IMAGES = {
-  hero: "/images/hero-kitchen-water-system.svg",
+  hero: heroImage,
   performance: "https://lh3.googleusercontent.com/aida-public/AB6AXuBRdlqqrlEa4eLqni6D2QwXPhc43n6A2Z4H2RPml36JlOqA2YtpjW6fFSY2p_cbCPHk61LYXtzT5pjlkgRF-4_IY8U_QF4RouA8og2_SHL_QjcU4etKQMviLC0vqGznewqzN_YiKgyL26TgWUh4IE5ZNbG4BM-ESpgytG-D1gSzdlY87BUaXlaAtZQ45JPcrKHRrrFD4Q100uSmqIanxJBnbdfI2yRStV6qFJMP78uLfAJ9HEhVUZa_BnHHISLtUUDrfK-sYFFN8MU",
   philosophy1: "https://lh3.googleusercontent.com/aida-public/AB6AXuCrB0JUtFq1rIXUCoSFZ5HbGPi-7nAw5_sd395V0Em2OOLkqXGW3GXsy31ROBPSxwx6RDL2Za2FTcdjdtbeBiJfZXKrU5cqP6TLQbjTDveyNt4_e4hSlt9JYGdjK3WC7rmakEG2K_OT8AeWYb6Ipb3djIL7dc78pKMiMDBavFaqhL6PwvYIE5Y6Ppvzx_9vhGcefFARmrUnV-lsxUC9d-JMLaLA7ZfB3Ec0-uPoPaUVj34g4WRed4JXwwEBmUglrADAckQJIBYSoxY",
   philosophy2: "https://lh3.googleusercontent.com/aida-public/AB6AXuDNq0D-tkQEugewUnTDvN9JOhJrs8cSkgk8VYnBIFKOdWyBnUsFBKRYbUNsKaCYnK5fEwuhDrmqwJLe41NGq2snO8Yt1-aGDBCXm91qEZMZ9EbJplwI7PUX3FXhVetB7i8wdEqRrIT76JUW9mLPlt1yXfSH3k0QJTHETvGe8sMElWWozKNR_2iQe5ezVl0Dxe5MNRBX8sykMXhdabgnuBlKfq3q_kpm2COG7T25D-71YI8PS3fON7b01ScaTdRjug8cXIHEEkZKEak"
